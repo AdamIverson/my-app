3986bfcf4e98c7339e062faf1b2c7c78f88dcc91
Adam Iverson <adamiverson@Adams-MacBook-Pro.local>
2022-03-05 12:04:11 -0600

    hello world and input box

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Main from "../Main/Main.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Main() {
+    
+    
+    return (
+        <>
+        <input></input>
+        <button>submit</button>
+        </>
+    )
+}
+
+export default Main;
